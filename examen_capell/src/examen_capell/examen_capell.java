bab7e00a94764a96dfd2756b1ad65e07afd08da3
USUARI <xaviercapell98@gmail.com>
2018-02-27 10:00:16 +0100

    examen

---
+++ examen_capell/src/examen_capell/examen_capell.java
@@ -0,0 +1,18 @@
+package examen_capell;
+
+public class examen_capell {
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+		
+	}
+	public static void printOne() {
+		System.out.println("Hello world");
+	}
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+
+}
